386e292c9e124cf38266e32163e9a04769c4242e
Carlo Ferrer <thecarloferrer@gmail.com>
2021-01-10 20:19:11 -0500

    Add work zhorts

---
+++ work.zsh
@@ -0,0 +1,15 @@
+alias cdw="cd ~/wave/src"
+alias cdwn="cd ~/wave/src/next-wave"
+
+alias jdp='jq .dependencies package.json'
+alias jsp='jq .scripts package.json'
+
+alias s="wave app:start"
+
+alias y="yarn"
+alias yd="yarn dev"
+alias ypf="yarn prettifile"
+alias ys="yarn storybook"
+alias yt="yarn test"
+alias ytd="yarn test-debug"
+alias ytw="yarn test --watch"
