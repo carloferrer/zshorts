934d3c15cc3ef6efc17fd0c9b0c8dabbfaa42efb
Carlo Ferrer <thecarloferrer@gmail.com>
2021-01-10 21:45:04 -0500

    Make installation foolproof

--- index.zsh
+++ index.zsh
@@ -1,24 +1,43 @@
 # Prepend source to .zshrc
+REPO_REMOTE="https://github.com/carloferrer/zhorts"
+REPO_LOCAL=$PWD/index.zsh
+SRC_STR="source ${REPO_LOCAL}"
 ZSH=~/.zshrc
-SRCPATH="source ${PWD}/index.zsh"
-REPO="https://github.com/carloferrer/zhorts"
 
-if grep -q $SRCPATH $ZSH
+# Print empty line for aesthetics
+echo ""
+
+# Check if index.zsh exists at current path
+if [ ! -f ./index.zsh ]
 then
-    echo "Source path to zhorts exists!"
+    echo "Couldn't find index.zsh at current path.\nNavigate to where the zhorts repo is cloned before installing!"
+
+# Check if index.zsh at current path is correct
+elif ! grep -q $REPO_REMOTE $REPO_LOCAL
+then
+    echo "Found an index.zsh file, but not the right one.\nNavigate to where the zhorts repo is cloned before installing!"
+
+# Check if zhorts is already installed
+elif grep -q $SRC_STR $ZSH
+then
+    echo "Zhorts is already installed!"
+
+# Install zhorts
 else
-    echo -e "# $REPO\n$SRCPATH\n\n$(cat $ZSH)" > $ZSH
+    # Prepend source to ~/.zshrc
+    echo -e "# $REPO_REMOTE\n$SRC_STR\n\n$(cat $ZSH)" > $ZSH
     echo "Source path to zhorts added!"
+
+    # Apply zhorts aliases etc.
+    for zhort in $PWD/*.zsh
+    do
+        if [ "${zhort}" != "${PWD}/index.zsh" ]
+        then
+            source $zhort
+        fi
+    done
+    echo "Zhorts installed!"
 fi
 
-# Apply zhorts aliases etc.
-for zhort in $PWD/*.zsh
-do
-    if [ "${zhort}" != "${PWD}/index.zsh" ]
-    then
-        source $zhort
-    fi
-done
-
-# Add alias to apply new aliases
+# Add alias to reload ~/.zshrc config
 alias sz="source ~/.zshrc"
