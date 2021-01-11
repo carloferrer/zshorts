884ebdaf55c31f7930adc9524559a8fca7ccb932
Carlo Ferrer <thecarloferrer@gmail.com>
2021-01-10 20:34:36 -0500

    Improve work zhorts

--- work.zsh
+++ work.zsh
@@ -1,11 +1,14 @@
 alias cdw="cd ~/wave/src"
 alias cdwn="cd ~/wave/src/next-wave"
 
+# Requires jq
 alias jdp='jq .dependencies package.json'
 alias jsp='jq .scripts package.json'
 
+# Requires Wave CLI
 alias s="wave app:start"
 
+# Requires yarn
 alias y="yarn"
 alias yd="yarn dev"
 alias ypf="yarn prettifile"
