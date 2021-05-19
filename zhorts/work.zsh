d0cc48d2fee1525112fdb748a5594e7a24b8c5cd
Carlo Ferrer <thecarloferrer@gmail.com>
2021-05-19 15:46:34 -0400

    Add new work zhorts

--- zhorts/work.zsh
+++ zhorts/work.zsh
@@ -1,21 +1,12 @@
-alias cdw="cd ~/wave/src"
-alias cdwn="cd ~/wave/src/next-wave"
+alias cdw="cd ~/wealthsimple"
 
 # Requires jq
 alias jdp="jq .dependencies package.json"
 alias jsp="jq .scripts package.json"
 
-alias m="make"
-alias mst="make ssh-test"
-
-# Requires Wave CLI
-alias s="wave app:start"
+alias wolfman="~/wealthsimple/wolfman/bin/wolfman"
 
 # Requires yarn
 alias y="yarn"
-alias yd="yarn dev"
-alias ypf="yarn prettifile"
 alias ys="yarn storybook"
 alias yt="yarn test"
-alias ytd="yarn test-debug"
-alias ytw="yarn test --watch"
