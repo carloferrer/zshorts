255010b75a82a55d0c1d230da7f5bbbb63f9ab15
Carlo Ferrer <thecarloferrer@gmail.com>
2022-07-22 09:44:33 -0400

    Add some aliases for git stuff

--- zshorts/personal.zsh
+++ zshorts/personal.zsh
@@ -1,9 +1,21 @@
 # Requires oh-my-zsh
 alias a='alias | grep'
 
-alias gcb='git checkout -b'
-alias ggf='git grep --files-with-matches'
-alias ggr='git grep'
+alias g='git'
+
+alias ga='g add'
+alias gc='g checkout'
+alias gcb='gc -b'
+alias gcam='g commit -am'
+alias gcm='gc master'
+alias gcmsg='g commit -m'
+alias gd='g diff'
+alias ggf='g grep --files-with-matches'
+alias ggr='g grep'
+alias gl='g pull'
+alias glo='g log -U0'
+alias gp='g push'
+alias gst='g status'
 
 # Python stuff
 alias pm='python manage.py'
