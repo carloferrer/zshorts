81f8c2c3050d548eca86db1c5a9b0f6a535e092a
Carlo <thecarloferrer@gmail.com>
2025-07-31 20:35:35 -0400

    Update some outdated stuff

--- zshorts/work.zsh
+++ zshorts/work.zsh
@@ -2,15 +2,9 @@
 alias jdp="jq .dependencies package.json"
 alias jsp="jq .scripts package.json"
 
-alias wolfman="~/wealthsimple/wolfman/bin/wolfman"
-
 # Requires yarn
 alias y="yarn"
 alias ys="yarn start"
 alias yt="yarn test"
 
 alias c="cd ~/code"
-
-# Requires nx
-alias ns="nx start"
-alias ntm="nx run mobile-micro-uis-money-movement:test --testFile="
