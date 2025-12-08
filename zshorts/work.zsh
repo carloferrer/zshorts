6389554a6bf872276c17e055d73e3128c775f9cb
Carlo Ferrer <cferrer@joinhomebase.com>
2025-12-08 15:35:01 -0500

    Update client-side commands

--- zshorts/work.zsh
+++ zshorts/work.zsh
@@ -3,8 +3,11 @@ alias jdp="jq .dependencies package.json"
 alias jsp="jq .scripts package.json"
 
 # Requires yarn
-alias y="yarn"
-alias ys="yarn start"
-alias yt="yarn test"
+# alias y="yarn"
+# alias ys="yarn start"
+# alias yt="yarn test"
 
 alias c="cd ~/Code"
+
+# Requires bun
+alias brd="bun run dev"
