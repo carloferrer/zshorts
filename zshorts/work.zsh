9f5726557b6e05e3041871ae6017ec50a1da04f5
Carlo Ferrer <cferrer@joinhomebase.com>
2025-12-12 11:28:51 -0500

    Remove redundant source

--- zshorts/work.zsh
+++ zshorts/work.zsh
@@ -7,8 +7,3 @@ alias c="cd ~/Code"
 # Requires bun
 alias brd="bun run dev"
 alias brt="bun run test"
-
-# Source local work aliases if file exists
-if [[ -f "${0:A:h}/work.local.zsh" ]]; then
-  source "${0:A:h}/work.local.zsh"
-fi
